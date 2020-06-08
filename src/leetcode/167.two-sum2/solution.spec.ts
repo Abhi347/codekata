1cc76a509f29bff9357891e05c2a9df26a2b3380
Abhishek Jain <abhishek.jain@omio.com>
2020-06-08 20:58:55 +0200

    test: Fixes test edge case

--- src/leetcode/167.two-sum2/solution.spec.ts
+++ src/leetcode/167.two-sum2/solution.spec.ts
@@ -6,7 +6,7 @@ describe('two sum solution', () => {
     expect(twoSum([3, 3], 6)).toEqual([1, 2]);
   });
   it('custom', () => {
-    expect(twoSum([2, 7, 7, 11, 15], 9)).toEqual([1, 2]);
+    expect(twoSum([2, 7, 7, 11, 15], 9)).toEqual([1, 3]);
     expect(twoSum([2, 7, 7, 11, 15], 14)).toEqual([2, 3]);
     expect(() => twoSum([2, 7, 7, 11, 15], 59)).toThrowError();
   });
