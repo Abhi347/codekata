ddc8e05d4e85237c330d847e0f1d2a181fb9a46b
Abhishek Jain <abhishek.jain@omio.com>
2020-06-08 19:32:35 +0200

    feat: Adds solution

--- src/leetcode/two-sum/solution.ts
+++ src/leetcode/two-sum/solution.ts
@@ -1,5 +1,16 @@
 export function twoSum(nums: number[], target: number): number[] {
-	//
+  const lookup: { [key: number]: number } = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    const complement = target - num;
+    if (complement in lookup) {
+      return [lookup[complement], i];
+    }
+    lookup[num] = i;
+  }
+  // edge case
+  throw new Error('No available solution');
 }
 
 export default twoSum;
